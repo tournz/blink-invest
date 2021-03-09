c23fb7c801b1f51bedc817f1c52a20c12ce3efbb
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-03-09 15:21:05 +0300

    ARCH: Created the routes for the main user stories

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :projects, only: [:index, :show] do
     resources :subscriptions, only: [:new, :create]
-    resources :messages, only: :index
-    resources :polls, only: [:new, :create, :index]
+  #  resources :messages, only: :index
+  #  resources :polls, only: [:new, :create, :index]
+  #  collection do
+  #    get :favorites
+  #  end
+  #  resources :favorites, only: [:create, :destroy]
   end
   resources :portfolio, only: :show
+  #resources :polls, only: [] do
+  #  resources :responses, only: :create
+  # end
+
 end
