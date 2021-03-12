c66cef2da219c81e506902da01a9a3aed580387f
boris nikiforov <nikiforov5000@gmail.com>
2021-03-12 13:57:31 +0300

    FIX chart are ok

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,5 +4,20 @@ class PortfoliosController < ApplicationController
     @projects_subscriptions_amount = Subscription.joins(:project).where(user: current_user, projects: { funded: false }).sum(&:amount)
     @confirmed_projects = current_user.projects.where(funded: true).distinct
     @confirmed_projects_subscriptions_amount = Subscription.joins(:project).where(user: current_user, projects: { funded: true }).sum(&:amount)
+
+    @subscriptions = current_user.subscriptions
+    @data = []
+    @subscriptions.each do |subscription|
+      data = []
+      # byebug
+      name = subscription.project.name
+      subscription.project.cash_yields.each do |cash_yield|
+        data << [cash_yield.date, cash_yield.value] if cash_yield.date < Date.today + 4.years
+      end
+      @data << {
+        name: name,
+        data: data
+      }
+    end
   end
-end
+  end
