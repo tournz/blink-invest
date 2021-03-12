3b2b3cae2242a0b04f931682cdb8af094178e6ad
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-03-12 12:46:21 +0300

    minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+
+  def sumproduct(a, b)
+    sum, i, n = 0, 0, a.size
+    while i < n
+      sum += a[i] * b[i]
+      i += 1
+    end
+    sum
+  end
+
 end
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,8 +1,17 @@
 class PortfoliosController < ApplicationController
   def show
     @projects = current_user.projects.where(funded: false).distinct
-    @projects_subscriptions_amount = Subscription.joins(:project).where(user: current_user, projects: { funded: false }).sum(&:amount)
+    @projects_subscriptions = Subscription.joins(:project).where(user: current_user, projects: { funded: false })
+
     @confirmed_projects = current_user.projects.where(funded: true).distinct
-    @confirmed_projects_subscriptions_amount = Subscription.joins(:project).where(user: current_user, projects: { funded: true }).sum(&:amount)
+    @confirmed_projects_subscriptions = Subscription.joins(:project).where(user: current_user, projects: { funded: true })
+    # byebug
+    # @net_cash_yield_average = sumproduct(@projects_subscriptions.amount)
+  end
+
+  private
+
+  def weighted_average(data, coefficients)
+    @projects.subscriptions
   end
 end
