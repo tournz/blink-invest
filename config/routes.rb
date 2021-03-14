d3aa7dd6f76685edadd42e6b6dc49959d46fd094
boris nikiforov <nikiforov5000@gmail.com>
2021-03-14 19:05:50 +0300

    ARCH: set root to home page, STYLE: made simle design for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'projects#index'
+  root :to => "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :projects, only: :show do
+  resources :projects, only: [:show, :index] do
     resources :subscriptions, only: [:new, :create]
   #  resources :messages, only: :index
   #  resources :polls, only: [:new, :create, :index]
