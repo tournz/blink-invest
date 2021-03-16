8cb57d9c1d4f1870c2ed7ee4a08189628afbb62a
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-03-16 17:33:26 +0300

    notifications when new subscription is made and project becomes funded

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,20 +1,24 @@
 class SubscriptionsController < ApplicationController
+  include ActionView::Helpers::NumberHelper
   before_action :set_project, only: [:new, :create]
-  before_action :set_user, only: :create
 
   def new
     @subscription = Subscription.new
-    @max_amount = @project.subscriptions.sum(&:amount)
+    @max_amount = @project.amount - @project.subscriptions.sum(&:amount)
     @users = @project.users.distinct
   end
 
   def create
     @subscription = Subscription.new(subscription_params)
     @subscription.project = @project
-    @subscription.user = @user
+    @subscription.user = current_user
     @users = @project.users.distinct
-    @max_amount = @project.subscriptions.sum(&:amount)
+    @max_amount = @project.amount - @project.subscriptions.sum(&:amount)
     if @subscription.save
+      @user_stake = current_user.subscriptions.where(project: @project).sum(&:amount).round(2)
+      @user_stake_percentage = (100 * @user_stake / @project.amount).round(2)
+      @percentage_funded = (100 * (@subscription.amount + @project.subscriptions.sum(&:amount)) / @project.amount).round(1)
+      Message.create(content: "Notification: #{current_user.name} has just made a subscription of #{number_to_currency(@subscription.amount)}. #{current_user.name}'s total stake is now #{number_to_currency(@user_stake)} amounting to #{@user_stake_percentage}% ownership. The project is now #{@percentage_funded}% funded.", user: User.find_by(name: 'Blink Bot'), chatroom: @project.chatroom)
       redirect_to portfolio_path
     else
       render :new
@@ -25,10 +29,6 @@ class SubscriptionsController < ApplicationController
 
   def set_project
     @project = Project.find(params[:project_id])
-  end
-
-  def set_user
-    @user = current_user
   end
 
   def subscription_params
