73cae49668074898bebe87c552f79bde9e34dea3
boris nikiforov <nikiforov5000@gmail.com>
2021-03-16 23:00:38 +0300

    FEAT: added map to index page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,13 @@ class ProjectsController < ApplicationController
     else
       @projects = Project.all
     end
+    @markers = @projects.geocoded.map do |project|
+      {
+        lat: project.latitude,
+        lng: project.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { project: project })
+      }
+    end
   end
 
   def show
