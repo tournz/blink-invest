cba24b3af059f96a37737f4ea707c8f416dbdef9
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-03-17 17:41:04 +0300

    fixed javascript

--- app/javascript/plugins/subscription.js
+++ app/javascript/plugins/subscription.js
@@ -6,25 +6,27 @@ const formatter = new Intl.NumberFormat('en-US', {
 
 const adjustFinancials = () => {
   const amountArea = document.querySelector("#subscription_amount");
-  amountArea.addEventListener("keyup", (event) => {
-    const subscriptionValue = parseFloat(amountArea.value);
-    const subscriptionDetails = document.querySelector("#subscription-details");
-    const projectAmount = subscriptionDetails.dataset.projectAmount;
-    const finalValue = subscriptionDetails.dataset.finalValue;
-    const lastYear = subscriptionDetails.dataset.lastYear;
-    const projectSurface = subscriptionDetails.dataset.surface;
-    console.log(projectAmount);
-    console.log(finalValue);
-    console.log(projectSurface);
-    console.log(lastYear);
-    const percentage = 100 * subscriptionValue / projectAmount;
-    const value = percentage * 0.01 * finalValue;
-    const surface = percentage * 0.01 * projectSurface;
-    const capitalGain = (value / subscriptionValue - 1) * 100
-    document.getElementById("subscription-percentage").innerHTML = `Amounts to ${percentage.toFixed(2)} % of the project`;
-    document.getElementById("final-capital-value").innerHTML = `Expected to turn into ${formatter.format(Math.round(value))} in ${lastYear} representing a ${capitalGain.toFixed(2)}% capital gain`;
-    document.getElementById("acre-surface").innerHTML = `Amounts to ${surface.toFixed(2)} acres`;
-  });
+  if (amountArea) {
+    amountArea.addEventListener("keyup", (event) => {
+      const subscriptionValue = parseFloat(amountArea.value);
+      const subscriptionDetails = document.querySelector("#subscription-details");
+      const projectAmount = subscriptionDetails.dataset.projectAmount;
+      const finalValue = subscriptionDetails.dataset.finalValue;
+      const lastYear = subscriptionDetails.dataset.lastYear;
+      const projectSurface = subscriptionDetails.dataset.surface;
+      console.log(projectAmount);
+      console.log(finalValue);
+      console.log(projectSurface);
+      console.log(lastYear);
+      const percentage = 100 * subscriptionValue / projectAmount;
+      const value = percentage * 0.01 * finalValue;
+      const surface = percentage * 0.01 * projectSurface;
+      const capitalGain = (value / subscriptionValue - 1) * 100
+      document.getElementById("subscription-percentage").innerHTML = `Amounts to ${percentage.toFixed(2)} % of the project`;
+      document.getElementById("final-capital-value").innerHTML = `Expected to turn into ${formatter.format(Math.round(value))} in ${lastYear} representing a ${capitalGain.toFixed(2)}% capital gain`;
+      document.getElementById("acre-surface").innerHTML = `Amounts to ${surface.toFixed(2)} acres`;
+    });
+  };
 };
 
 export { adjustFinancials }
